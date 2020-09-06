2df86322339f113e9a9b7b844870e23e0febea58
Sebastián Gálvez <sebastian.galvez.milla@gmail.com>
2020-09-05 20:50:27 -0400

    Segundo cambio

--- operaciones.py
+++ operaciones.py
@@ -2,4 +2,9 @@ x=3+4
 print(f'La suma 3+4 es igual a {x}')
 y=2*5
 print(f'La multiplicación 2*5 es {y}')
-
+z=5-3
+print(f'La resta 5-3 es igual a {z}')
+w=9/2
+print(f'La división 9/2 es {w}')
+f=9%2
+print(f'El módulo, es decir, el resto de la división 9/2 es {f}')
